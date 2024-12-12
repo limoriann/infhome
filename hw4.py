4727fc021579913169fab0683a22f13b75b58b9e
ann <aazernova06@gmail.com>
2024-12-13 01:59:43 +0300

    Update hw4.py

--- hw4.py
+++ hw4.py
@@ -1,3 +1,4 @@
+#Задание 1
 a = int(input())
 s=0
 if a > 100:
@@ -6,3 +7,14 @@ else:
 	for i in range(1, a+1):
 		s+=i**2
 	print(s)
+#Задание 2
+x = [ ]
+for i in range(1, 10):
+	y = [ ]
+	for j in range(1, 10):
+		y.append(str(i*j)+(3 - len(str(i*j)))*' ')
+	x.append(y)
+for i in range(9):
+	print(*x[i], end = '\n\n')
+	
+	
